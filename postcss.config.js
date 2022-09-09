f15e84dbdf210832c1e18f8c4ad0023fce441489
xyj <296576733@qq.com>
2022-09-09 18:41:54 +0800

    0.0

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+/*
+ * @Descripttion: 
+ * @Author: BZR
+ * @Date: 2022-06-22 10:19:22
+ * @LastEditTime: 2022-06-22 10:19:27
+ */
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+}
